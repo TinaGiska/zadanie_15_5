60fcc6ff54e217741b748ccb69b761a97e797077
Tina Giska <tina.tylicka@gmail.com>
2018-06-24 12:01:43 +0200

    Create task

---
+++ components/App.js
@@ -0,0 +1,94 @@
+var GIPHY_API_URL = 'https://api.giphy.com';
+const GIPHY_PUB_KEY='dmEpis77lFhUpd34BHhiTLz9jP4sGwa7';
+
+App = React.createClass({
+   getInitialState() {
+       return {
+           loading: false,
+           searchingText: '',
+           gif: {}
+       };
+   },
+
+   handleSearch: function(searchingText) {
+       this.setState({
+           loading: true
+       });
+
+       this.getGif(searchingText)
+           .then(function(gif) {
+               this.setState({
+                   loading: false,
+                   gif: gif,
+                   searchingText: searchingText
+               });
+           }.bind(this))
+           .catch(function(err) {
+
+           });
+     },
+/*
+   getGif: function(searchingText, callback) {
+       var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+       var xhr = new XMLHttpRequest();
+       xhr.open('GET', url);
+       xhr.onload = function() {
+           if (xhr.status === 200) {
+               var data = JSON.parse(xhr.responseText).data;
+               var gif = {
+                   url: data.fixed_width_downsampled_url,
+                   sourceUrl: data.url
+               };
+               callback(gif);
+           }
+       };
+       xhr.send();
+   },
+   */
+   getGif: function(searchingText){
+       return new Promise(function(resolve, reject){  //
+           var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+           var xhr = new XMLHttpRequest();
+
+           xhr.open('GET', url);
+           xhr.onload = function() {
+
+               if (xhr.status === 200) {
+                   var data = JSON.parse(xhr.responseText).data;
+                   var gif = {
+                       url: data.fixed_width_downsampled_url,
+                       sourceUrl: data.url
+                   };
+                   resolve(gif);
+               }
+               else {
+                   reject(new Error('Gif not found'));
+               }
+           };
+           xhr.send();
+       });
+   },
+
+   render: function() {
+
+       var styles = {
+           margin: '0 auto',
+           textAlign: 'center',
+           width: '90%'
+       };
+
+       return (
+           <div style={styles}>
+               <h1>Wyszukiwarka GIFow!</h1>
+               <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, by pobrać kolejne gify.</p>
+               <Search onSearch={this.handleSearch} />
+             <Gif
+               loading={this.state.loading}
+               url={this.state.gif.url}
+               sourceUrl={this.state.gif.sourceUrl}
+             />
+           </div>
+       );
+   }
+
+});
